68306979ff4300c45c1b5cefd0abdd78f8ae91a9
Danny <dz37@rice.edu>
2022-05-18 16:33:26 -0500

    Finishing touches, added some styling to make it look dope.

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function generateSquares(size) {
             let square = document.createElement('square');
             container.appendChild(square);
             square.addEventListener('mouseover', function() {
-            square.setAttribute("style", `background-color:blue; width:${960/size}, height:${960/size}`);
+            square.setAttribute("style", `background-color:blue; width:${700/size}, height:${700/size}`);
             console.log('hovering')})
         }
     }
